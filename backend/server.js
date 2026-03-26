feb5f782c9a8b3bbce7b33187d4030e55c9e3d73
Rohit Salunke <rohitsalunke107@gmail.com>
2026-03-27 02:11:42 +0530

    backend is completed all info is now send to mongoDb atlas

--- backend/server.js
+++ backend/server.js
@@ -2,18 +2,20 @@ import express from "express";
 import dotenv from "dotenv";
 import connectDB from "./config/db.js";
 import contactRoutes from "./routes/contactRoutes.js";
+import cors from "cors";
 
 dotenv.config();
 const app = express();
 connectDB();
 
 app.use(express.json());
-
+app.use(cors());
 app.use("/api", contactRoutes);
 
 app.get("/", (req, res) => {
   res.send("Portfolio Backend Running ✅");
 });
+
 
 app.listen ( process.env.PORT , ()=>{
     console.log(`port is connected on ${process.env.PORT}`)
