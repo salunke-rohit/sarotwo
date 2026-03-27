1d345a99a879ee7bc5ce25e97d5764c03dfee4e6
Rohit Salunke <rohitsalunke107@gmail.com>
2026-03-27 23:38:29 +0530

    connect frontend to deployed backend

--- frontend/src/pages/contact/Contact.jsx
+++ frontend/src/pages/contact/Contact.jsx
@@ -21,7 +21,7 @@ export default function Contact() {
 
     try {
       const res = await axios.post(
-        "http://localhost:5000/api/contact",
+        "https://sarotwo-backend.onrender.com/api/contact",
         formData
       );
 
